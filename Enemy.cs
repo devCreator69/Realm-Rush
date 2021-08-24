82f7bed8b60c0fccfa0322f7f62deb5dd872d04e
devCreator69 <81888953+devCreator69@users.noreply.github.com>
2021-08-24 12:29:26 -0400

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int goldReward = 25;
+    [SerializeField] int goldPenalty = 25;
+
+    Bank bank;
+
+    void Start()
+    {
+        bank = FindObjectOfType<Bank>();
+    }
+
+    public void RewardGold()
+    {
+        if (bank == null) {return; }
+        // added if statement to be sure that the bank script is really being called (just a safegaurd)
+        bank.Deposit(goldReward);
+    }
+
+     public void StealGold()
+    {
+        if (bank == null) {return; }
+        bank.Withdraw(goldPenalty);
+    }
+
+ 
+}
